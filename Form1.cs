bcd31641ad8e5e3d4cdabed1662a23e7e405f21d
Patryk Jasik <przedmioty.jasik@gmail.com>
2016-11-30 10:40:09 +0100

    button was added

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace test_Git
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Zmiana tekstu";
+        }
     }
 }
